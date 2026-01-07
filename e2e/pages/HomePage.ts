5a118c4a29ef2b2e63fdcdb0bb904406a54439c3
Rocco Salvetti <38426041+PimySoft@users.noreply.github.com>
2026-01-07 20:15:59 +0000

    Add compiler tests and refactor HomePage selectors
    - Add new compiler.spec.ts with compiler page tests
    - Remove failing WebView test from compiler suite
    - Refactor HomePage.ts: use accessibility IDs, reduce repetition
    - Convert all selectors to use ~ prefix for accessibility IDs
    - Group selectors with comments for better organization

--- e2e/pages/HomePage.ts
+++ e2e/pages/HomePage.ts
@@ -1,53 +1,23 @@
 import { BasePage } from './BasePage';
 
 export class HomePage extends BasePage {
-  get semester1() {
-    return this.driver.$('~Semester1');
-  }
+  // Semester cards
+  get semester1() { return this.driver.$('~Semester1'); }
+  get semester2() { return this.driver.$('~Semester2'); }
+  get semester3() { return this.driver.$('~Semester3'); }
+  get semester4() { return this.driver.$('~Semester4'); }
+  get semester5() { return this.driver.$('~Semester5'); }
+  get semester6() { return this.driver.$('~Semester6'); }
 
-  get semester2() {
-    return this.driver.$('~Semester2');
-  }
+  // Navigation buttons
+  get interview() { return this.driver.$('~Interview'); }
+  get blog() { return this.driver.$('~Blog'); }
+  get compiler() { return this.driver.$('~Compiler'); }
 
-  get semester3() {
-    return this.driver.$('~Semester3');
-  }
-
-  get semester4() {
-    return this.driver.$('~Semester4');
-  }
-
-  get semester5() {
-    return this.driver.$('~Semester5');
-  }
-
-  get semester6() {
-    return this.driver.$('~Semester6');
-  }
-
-  get interview() {
-    return this.driver.$('~Interview');
-  }
-
-  get blog() {
-    return this.driver.$('~Blog');
-  }
-
-  get compiler() {
-    return this.driver.$('~Compiler');
-  }
-
-  get rateUs() {
-    return this.driver.$('~RateUs');
-  }
-
-  get share() {
-    return this.driver.$('~Share');
-  }
-
-  get contribute() {
-    return this.driver.$('~Contribute');
-  }
+  // Action buttons
+  get rateUs() { return this.driver.$('~RateUs'); }
+  get share() { return this.driver.$('~Share'); }
+  get contribute() { return this.driver.$('~Contribute'); }
 
   async openSemester(semester: 1 | 2 | 3 | 4 | 5 | 6): Promise<void> {
     const semesterMap = {
